0b0bf0810ee8d28adac5422bb39e9eda4b4e0512
ElisangelaWendt <61567233+ElisangelaWendt@users.noreply.github.com>
2021-11-10 13:43:17 -0300

    Ajustes para exibir imagens

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'documentacao.apps.DocumentacaoConfig',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets/static')
 ]
 
 MIDDLEWARE = [
@@ -52,7 +57,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-import os
 
 TEMPLATES = [
     {
